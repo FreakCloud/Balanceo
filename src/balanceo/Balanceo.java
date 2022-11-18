2763abd3ecb788d9e838cddc7afdc078bb9827f4
FreakCloud <111830587+FreakCloud@users.noreply.github.com>
2022-11-17 22:26:25 -0600

    Update Balanceo.java

--- src/balanceo/Balanceo.java
+++ src/balanceo/Balanceo.java
@@ -15,7 +15,43 @@ public class Balanceo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         FileReader archivo; 
+        BufferedReader lector;
+        int contador =0;
+        
+        try{
+            archivo = new FileReader("c:\\texto1.txt");
+            if(archivo.ready()){
+                lector = new BufferedReader(archivo);
+                String cadena;
+                while((cadena= lector.readLine()) != null){
+                    Pila<String> pila = new Pila<>();
+                    
+                    for(int i=0; i<cadena.length(); i++){
+                        if(cadena.charAt(i)=='('){
+                            pila.push('(');
+                            contador ++;
+                        } else { if(cadena.charAt(i)==')') {
+                            pila.pop();
+                            contador ++; 
+                        } 
+                        }
+                    }
+                    if(pila.isEmpty() & contador%2==0){
+                        System.out.print("Correcto");
+                    } else System.out.print("InCorrecto");
+                    
+                    
+                    
+                }
+            } else { 
+                System.out.println("El archivo no fue encontrado");
+            }
+        }
+        
+        
+        
+        
     }
     
 }
