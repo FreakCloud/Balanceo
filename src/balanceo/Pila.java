7d9ed1a53a2f7807eb715eef6802fba0e8b785e3
FreakCloud <111830587+FreakCloud@users.noreply.github.com>
2022-11-17 22:27:03 -0600

    Update Pila.java

--- src/balanceo/Pila.java
+++ src/balanceo/Pila.java
@@ -9,6 +9,54 @@ package balanceo;
  *
  * @author lopez
  */
-public class Pila {
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lopez
+ */
+public class Pila <T>{
+   ArrayList<T> datos = new ArrayList<>();
+    private int top;
+
+    public Pila() {
+    }
     
+    public void push(T dato){
+        datos.add(dato);
+        top++;
+    }
+    
+    public int lenght(){
+        return this.top;
+    }
+    
+    public boolean isEmpty(){
+        return this.datos==null;
+    }
+    
+    public T pop(){
+        T dato = datos.remove(top-1);
+        top--;
+        return dato;
+    }
+    
+    public T peek(){
+        T dato = datos.get(top-1);
+        return dato;
+    }
+    
+    @Override
+    public String toString(){
+        String estado = "";
+        
+        for (T dato : datos) {
+            estado += dato + " ->" + " " ;             
+        }
+        return estado;
+    }
+
+    void push(char c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
